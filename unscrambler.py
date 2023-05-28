06cf3c45fd58f697bf5341e9dc1fc8d5c1925609
Kenneth Muuli <kenneth.muuli@gmail.com>
2023-05-28 22:01:29 +0300

    Fixed solution

--- unscrambler.py
+++ unscrambler.py
@@ -1,58 +1,44 @@
-# wleoly
-# ucmrsa
-# utoshon
-# rsoslaoc
-# kumfce
-# sdeefen
-# moraac
-# rrgalwne
-# isnsna
-# oimterrm
+import sys
 
-from collections import Counter
+class Unscrambler:
 
-clearedLines = []
-clearedWLLines = []
-
-# --- Read in .txt file, clear contents of linebreaks -> "\n" and save into a list variable
-def ReadAndClearFromTxt(fileName, outputListName):
-    with open(fileName, "r") as f:
-        lines = f.readlines()
-
-    linesLength = len(lines)
-
-    for i in range(linesLength):
-        outputListName.append(lines[i].replace("\n", ""))
-        
-def MatchWord(currentWord):
-    charDict = Counter(currentWord)
-    lastDictKey = list(charDict)[-1]
-
-    for i in range(len(clearedWLLines)):
-        compWord = clearedWLLines[i]
-        if len(compWord) != len(currentWord):
-            continue
-        else:
-            for x in charDict.keys():
-                if x not in compWord:
-                    break
-                else: 
-                    print(x)
-                    for y in charDict.values():
-                        if compWord.count(x) != y:
-                            break
-                        else:
-                            if compWord.count(x) == y and x == lastDictKey:
-                                return (compWord)
-
-ReadAndClearFromTxt("input_example.txt", clearedLines)
-ReadAndClearFromTxt("wordlist.txt", clearedWLLines)
-
-answerWordList = []
-
-for i in range(len(clearedLines)):
-    currentWord = clearedLines[i]
-    matchWord = MatchWord(currentWord)
-    answerWordList.append(matchWord)
+    WORD_LIST_FILENAME = "wordlist.txt" #Const
     
-print(answerWordList)
+    world_list_original = []
+    word_list_dict= {}
+    
+    def __init__(self):
+        self.world_list_original = self.read_and_clear_txt(self.WORD_LIST_FILENAME)
+        self.create_wordlist_dictionary()
+
+    # Read in .txt file, clear contents of linebreaks -> "\n" and save into a list variable
+    def read_and_clear_txt(self, file_name):
+        f = open(file_name, "r")
+        values = f.read().rstrip().split("\n")
+        return values
+
+    # Create wordlist dict
+    def create_wordlist_dictionary(self):
+        for i in range(len(self.world_list_original)):
+            self.word_list_dict[''.join(sorted(self.world_list_original[i]))] = self.world_list_original[i]
+            
+
+    # Read in input txt
+    def find_matches(self, input):
+        answer = []
+        for i in range (len(input)):
+            # sorteeri input sõnad üks haaval ja võrdle sorteeritud vastet wordlist dict key-ga
+            sortedInputWord = ''.join(sorted(input[i]))
+            if self.word_list_dict.get(sortedInputWord):
+                # kui leiab key vaste, siis lisa sõna answer listi
+                answer.append(self.word_list_dict.get(sortedInputWord))
+        return answer
+                
+    def run(self, file_name):
+        input = self.read_and_clear_txt(file_name)
+        answer = self.find_matches(input)
+        print(','.join((answer)))
+
+unscrambler = Unscrambler()
+
+unscrambler.run(sys.argv[1])
